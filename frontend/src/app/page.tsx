46a35f950e06ffe67442a7bbb915919db3b374b0
luiizsilverio <luiiz.silverio@gmail.com>
2024-10-01 11:31:35 -0300

    Aula 01 até 01:21:00

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,13 @@
+import ProdutoItem from "@/components/produto/ProdutoItem";
+import { produtos } from "@/core";
 
 export default function Home() {
   return (
-    <div>Gamer Store</div>
+    <div className='grid grid-cols-4 container'>
+      {produtos.map((prod, index) => {
+        if (index < 7)
+          return <ProdutoItem key={prod.id} produto={prod} />
+      })}
+    </div>
   );
 }
---
+++ frontend/src/components/produto/ProdutoItem.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import { IconShoppingCartPlus } from '@tabler/icons-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Moeda, Produto } from '@/core';
+import NotaReview from '@/components/NotaReview';
+
+export interface ProdutoItemProps {
+    produto: Produto;
+}
+
+export default function ProdutoItem(props: ProdutoItemProps) {
+  const { produto } = props;
+
+  return (
+    <Link
+      href={`/produto/${props.produto.id}`}
+      className="flex flex-col bg-violet-dark border border-white/10 rounded-xl relative max-w-[350px]"
+    >
+      <div className="absolute flex justify-end top-2.5 right-2.5">
+        <NotaReview nota={props.produto.nota} />
+      </div>
+
+      <div className="w-full h-48 relative">
+        <Image
+          src={produto.imagem}
+          fill
+          className="object-contain"
+          alt="Imagem do Produto"
+        />
+      </div>
+
+      <div className="flex-1 flex flex-col justify-between gap-3 p-5 border-t border-white/10">
+        <span className="text-lg font-semibold">{produto.nome}</span>
+        <div className="self-start text-sm border-b border-dashed">
+          <span className='whitespace-nowrap'>{produto.especificacoes.destaque}</span>
+        </div>
+
+        {/* <div className="flex-1"></div> */}
+        <div className="flex flex-col">
+          <span className="text-sm text-gray-400 line-through">
+              de {Moeda.formatar(produto.precoBase)}
+          </span>
+          <span className="text-xl font-semibold text-emerald-400">
+              por {Moeda.formatar(produto.precoPromo)}
+          </span>
+          <span className="text-zinc-400 text-xs">
+            até 6x de {Moeda.formatar(1.5)}
+          </span>
+        </div>
+
+        <button
+          className={`
+            flex justify-center items-center gap-2 h-8 text-xs lg:text-base
+            bg-violet-700 hover:border-2 border-emerald-500 rounded-full
+          `}
+          onClick={(e) => {
+            e.preventDefault()
+            console.log('Adicionar ao carrinho')
+            // adicionarItem(props.produto)
+          }}
+        >
+          <IconShoppingCartPlus size={20} />
+          <span className='hidden sm:block'>Adicionar</span>
+        </button>
+      </div>
+    </Link>
+  )
+}
